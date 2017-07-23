435feea4d9a6d787df73f5d239840a11acb0aa4d
Luca Tabone <taboneluca3@gmail.com>
2017-07-23 19:01:24 +0200

    Refactor code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 'use strict'
 
-;(async function () {
+;(async () => {
   /**
    * Root HTML Element.
    * @type {Object}
@@ -8,10 +8,10 @@
   const rootElement = document.querySelector('.app')
 
   // Register all components.
-  (await require('./components')).forEach(component => {
+  ;(await require('./components')).forEach(component => {
     Vue.component(component.name, component.options)
   })
   
   // Initialize Vue instance.
-  const vm = new Vue({ el: rootElement, data: { page: 'page-one' }})
-}())
+  const vm = new Vue({ el: rootElement, data: { page: 'tasks' }})
+})()
